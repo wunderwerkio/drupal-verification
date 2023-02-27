b9c7f1b3006daaa238d7379b6b894542fed50c25
chfoidl <chfoidl@3686637.no-reply.drupal.org>
2023-02-27 14:58:25 +0100

    Fix CS

--- verification.api.php
+++ verification.api.php
@@ -5,15 +5,17 @@
  * Hooks specific to the Verification API module.
  */
 
+use Drupal\Core\Session\AccountInterface;
+
 /**
  * Alter the supported operations of the Hash Verification Provider.
  *
  * @param array $operations
  *   The operations to be altered.
  *
- * @see Drupal\verification\Plugin\VerificationProvider\Hash::getSupportedOperations().
+ * @see Drupal\verification\Plugin\VerificationProvider\Hash::getSupportedOperations()
  */
-function hook_verification_provider_hash_supported_operations_alter(&$operations) {
+function hook_verification_provider_hash_supported_operations_alter(array &$operations) {
   $operations[] = 'new-operation';
 }
 
@@ -30,9 +32,9 @@ function hook_verification_provider_hash_supported_operations_alter(&$operations
  * @param \Drupal\Core\Session\AccountInterface $user
  *   The user performing the operation.
  *
- * @see Drupal\verification\Plugin\VerificationProvider\Hash::getTimeout().
+ * @see Drupal\verification\Plugin\VerificationProvider\Hash::getTimeout()
  */
-function hook_verification_provider_hash_timeout_alter(&$timeout, $operation, $user) {
+function hook_verification_provider_hash_timeout_alter(&$timeout, $operation, AccountInterface $user) {
   if ($operation === 'some-special-operation') {
     $timeout = 60;
   }
